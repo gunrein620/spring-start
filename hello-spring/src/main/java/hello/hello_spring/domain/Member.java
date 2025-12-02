de12de76acd87afb46d4f547f37e64f28d8da4dc
gunrein <gunrein620@gmail.com>
2025-12-02 15:00:19 +0900

    feat: 회원도메인 및 레포지토리 생성

---
+++ hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -0,0 +1,19 @@
+package hello.hello_spring.domain;
+
+public class Member  {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
